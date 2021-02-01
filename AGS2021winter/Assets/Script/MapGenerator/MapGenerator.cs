4cf7bdb8f647401825c0ae404ca81e3f31ff75c5
nitta <hiromu.1999.1227@gmail.com>
2021-02-01 17:42:19 +0900

    9x9チャンクのマップ生成 中央端から順番に生成

--- AGS2021winter/Assets/Script/MapGenerator/MapGenerator.cs
+++ AGS2021winter/Assets/Script/MapGenerator/MapGenerator.cs
@@ -14,7 +14,9 @@ public class MapGenerator : MonoBehaviour
     }
 
     public Vector3Int mapSize;
-    public Transform[] block;
+    public Vector2Int worldSize;
+    public GameObject[] block;
+    GameObject chunk;
 
     BlockID[][][] mapData;
     // Start is called before the first frame update
@@ -22,42 +24,34 @@ public class MapGenerator : MonoBehaviour
     {
         GameObject floorplane = GameObject.CreatePrimitive(PrimitiveType.Plane);
         GameObject ceilingplane = GameObject.CreatePrimitive(PrimitiveType.Plane);
-        //obj.transform.position = new Vector3((mapSize.x * block[0].localScale.x * 4)/2, mapSize.y * block[0].localScale.y * 4, (mapSize.z * block[0].localScale.z * 4)/2);
-        floorplane.transform.localScale = new Vector3(0.4f*mapSize.x,1.0f,0.4f*mapSize.z);
-        ceilingplane.transform.localScale = new Vector3(0.4f*mapSize.x,1.0f,0.4f*mapSize.z);
-        floorplane.transform.position = new Vector3(0,0,0);
-        ceilingplane.transform.position = new Vector3(0,8.0f,0);
-        ceilingplane.transform.rotation = new Quaternion( 0, 0,90,1);
+        //obj.transform.position = new Vector3((mapSize.x * block[0].transform.localScale.x * 4)/2, mapSize.y * block[0].transform.localScale.y * 4, (mapSize.z * block[0].transform.localScale.z * 4)/2);
+        floorplane.transform.localScale = new Vector3(0.4f * mapSize.x, 1.0f, 0.4f * mapSize.z);
+        ceilingplane.transform.localScale = new Vector3(0.4f * mapSize.x, 1.0f, 0.4f * mapSize.z);
+        floorplane.transform.position = new Vector3(0, 0, 0);
+        ceilingplane.transform.position = new Vector3(0, 8.0f, 0);
+        ceilingplane.transform.rotation = new Quaternion(0, 0, 90, 1);
         //Instantiate(obj,, Quaternion.identity);
 
-        MapInit(); 
-                     
-        for (int x = 0; x < mapSize.x; x++)
+        for(int x = 0;x < worldSize.x;x++)
         {
-            for (int y = 0; y < mapSize.y; y++)
+            for (int y = 0; y < worldSize.x; y++)
             {
-                for (int z = 0; z < mapSize.z; z++)
-                {
-                    var id = mapData[x][y][z];switch (id)
-                    {
-                        case BlockID.air:
-                            break;
-                        case BlockID.stone:
-                            Instantiate(block[0], new Vector3((mapSize.x * (block[0].localScale.x * 4))/2-(x *(block[0].localScale.x * 4))-2, (y * (block[0].localScale.y * 4)), (mapSize.z * (block[0].localScale.x * 4)) / 2 - (z * (block[0].localScale.z * 4)) - 2), Quaternion.identity);
-                             break;
-                        case BlockID.rail:
-                            Instantiate(block[1], new Vector3((mapSize.x * (block[1].localScale.x * 4))/2 - (x * (block[1].localScale.x * 4)) - 2, (y * (block[1].localScale.y * 4)), (mapSize.z * (block[1].localScale.z * 4)) / 2 - (z * (block[1].localScale.z * 4)) - 2), Quaternion.identity);
-                            break;
-                        default:
-                            Assert.IsFalse(id == BlockID.max, "BlockIDに想定されていない値が入っています。");
-                            break;
-                    }
-                }
+                MapInit(x);
+                chunk = new GameObject("Chunk" + x.ToString() + y.ToString());
+                MapGenerate();
+                var tmpstone = block[0].transform.Find("tmpstone");
+                Debug.Log(tmpstone);
+                var tmpdefult = tmpstone.transform.Find("default").gameObject;
+                Debug.Log(tmpdefult);
+                Mesh mesh = tmpdefult.GetComponent<MeshFilter>().sharedMesh;
+                chunk.transform.position = new Vector3((mapSize.x * mesh.bounds.size.x) * (((x+1)/2)*((((x+1)%2)*2)-1)), 0.0f, (mapSize.z * mesh.bounds.size.z) * y);
             }
         }
     }
 
-    void MapInit()
+
+
+    void MapInit(int chunkCnt)
     {
         mapData = new BlockID[mapSize.x][][];
         for (int x = 0; x < mapSize.x; x++)
@@ -68,39 +62,77 @@ public class MapGenerator : MonoBehaviour
                 mapData[x][y] = new BlockID[mapSize.z];
                 for (int z = 0; z < mapSize.z; z++)
                 {
-                    mapData[x][y][z] = BlockID.max;
+                    if (chunkCnt == 0)
+                    {
+                        mapData[x][y][z] = BlockID.max;
+                    }
+                    else
+                    {
+                        mapData[x][y][z] = BlockID.stone;
+                    }
                 }
             }
         }
+        if (chunkCnt == 0)
+        {
+            for (int x = 0; x < mapSize.x; x++)
+            {
+                for (int y = 0; y < mapSize.y; y++)
+                {
+                    for (int z = 0; z < mapSize.z; z++)
+                    {
+                        if (mapData[x][y][z] != BlockID.max)
+                        {
+                            continue;
+                        }
+                        mapData[x][y][z] = BlockID.stone;
+                        if (x == mapSize.x / 2)
+                        {
+                            if (y == 0)
+                            {
+                                mapData[x][y][z] = BlockID.rail;
+                            }
+                            else
+                            {
+                                mapData[x][y][z] = BlockID.air;
+                            }
+                            mapData[x - 1][y][z] = BlockID.air;
+                            mapData[x + 1][y][z] = BlockID.air;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    void MapGenerate()
+    {
         for (int x = 0; x < mapSize.x; x++)
         {
             for (int y = 0; y < mapSize.y; y++)
             {
                 for (int z = 0; z < mapSize.z; z++)
                 {
-                    if (mapData[x][y][z] != BlockID.max)
+                    var id = mapData[x][y][z]; switch (id)
                     {
-                        continue;
-                    }
-                    mapData[x][y][z] = BlockID.stone;
-                    if (x == mapSize.x / 2)
-                    {
-                        if (y == 0)
-                        {
-                            mapData[x][y][z] = BlockID.rail;
-                        }
-                        else
-                        {
-                            mapData[x][y][z] = BlockID.air;
-                        }
-                        mapData[x - 1][y][z] = BlockID.air;
-                        mapData[x + 1][y][z] = BlockID.air;
+                        case BlockID.air:
+                            break;
+                        case BlockID.stone:
+                            var stone = Instantiate(block[0], new Vector3((mapSize.x * (block[0].transform.localScale.x * 4)) / 2 - (x * (block[0].transform.localScale.x * 4)) - 2, (y * (block[0].transform.localScale.y * 4)), (mapSize.z * (block[0].transform.localScale.x * 4)) / 2 - (z * (block[0].transform.localScale.z * 4)) - 2), Quaternion.identity);
+                            stone.transform.parent = chunk.transform;
+                            break;
+                        case BlockID.rail:
+                            var rail = Instantiate(block[1], new Vector3((mapSize.x * (block[1].transform.localScale.x * 4)) / 2 - (x * (block[1].transform.localScale.x * 4)) - 2, (y * (block[1].transform.localScale.y * 4)), (mapSize.z * (block[1].transform.localScale.z * 4)) / 2 - (z * (block[1].transform.localScale.z * 4)) - 2), Quaternion.identity);
+                            rail.transform.parent = chunk.transform;
+                            break;
+                        default:
+                            Assert.IsFalse(id == BlockID.max, "BlockIDに想定されていない値が入っています。");
+                            break;
                     }
                 }
             }
         }
-        
     }
+    
     // Update is called once per frame
     void Update()
     {
