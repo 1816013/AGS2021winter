53610852d49a8dafd5a969c2027b0a361a64070a
1816013 <iahliugaeligfaliugefliw.com>
2021-02-01 14:10:19 +0900

    動くプレイヤー(仮)

---
+++ AGS2021winter/Assets/Scenes/Test/gameMain.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameMain : MonoBehaviour
+{
+    public GameObject prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 10000; i++)
+        {
+
+            Instantiate(prefab);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
