9366bb69cbbbd7e336eab91ee0e54cd08dfef80e
1816013 <iahliugaeligfaliugefliw.com>
2021-02-19 10:02:30 +0900

    プレイヤー移動改
    プレイヤー 1を使ってください

--- AGS2021winter/Assets/Script/Test/player.cs
+++ AGS2021winter/Assets/Script/Test/player.cs
@@ -4,32 +4,50 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
+	private GameObject body;
     // Start is called before the first frame update
     void Start()
     {
-        
+		body = this.transform.Find("body").gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
+		Vector3 move = new Vector3(0,0,0);
+		float step = 2 * Time.deltaTime;
 		if (Input.GetKey(KeyCode.W))
 		{
-			transform.position += transform.forward * 2 * Time.deltaTime;
-		}
-		if (Input.GetKey(KeyCode.S))
-		{
-			transform.position -= transform.forward * 2 * Time.deltaTime;
-		}
-		if (Input.GetKey(KeyCode.D))
-		{
-			transform.Rotate(new Vector3(0, 1, 0));
-			transform.position += transform.right * 2 * Time.deltaTime;
+			//body.transform.rotation = Quaternion.Slerp(body.transform.rotation, Quaternion.Euler(0, 0, 0), step);
+			move += Vector3.forward * 2 * Time.deltaTime;
 		}
 		if (Input.GetKey(KeyCode.A))
 		{
-			transform.Rotate(new Vector3(0, -1, 0));
-			transform.position -= transform.right * 2 * Time.deltaTime;
+			//body.transform.rotation = Quaternion.Slerp(body.transform.rotation, Quaternion.Euler(0, 270.0f, 0), step);
+			move -= Vector3.right * 2 * Time.deltaTime;
 		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			//body.transform.rotation = Quaternion.Slerp(body.transform.rotation, Quaternion.Euler(0, 180.0f, 0), step);
+			move -= Vector3.forward * 2 * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			//body.transform.rotation = Quaternion.Slerp(body.transform.rotation, Quaternion.Euler(0, 90.0f, 0), step);
+			move += Vector3.right * 2 * Time.deltaTime;
+		}
+		float angle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg;
+		if (move != Vector3.zero)
+		{
+			float mathAngle = Mathf.Abs(body.transform.rotation.eulerAngles.y - angle);
+			Debug.Log(mathAngle);
+			mathAngle = mathAngle > 180.0f ? Mathf.Abs(mathAngle - 360.0f) : mathAngle;
+			
+			float parsent = mathAngle / 180.0f;
+			float speed = Mathf.Max(30.0f * parsent, 1.0f);
+			
+			body.transform.rotation = Quaternion.RotateTowards(body.transform.rotation, Quaternion.Euler(0, angle, 0), speed);
+		}
+		transform.position += move;
 	}
 }
