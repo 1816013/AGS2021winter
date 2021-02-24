54db8e8007d774a91cd5a832285ad774fd29469f
1816013 <iahliugaeligfaliugefliw.com>
2021-02-25 07:30:06 +0900

    追加漏れ

---
+++ AGS2021winter/Assets/Script/MapGenerator/ObjCtr.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjCtr : MonoBehaviour
+{
+    public MapGenerator mapGen;
+    List<GameObject> objs = new List<GameObject>();
+
+    // Update is called once per frame
+    void Update()
+    {
+        // マップデータが出来てないとき
+        if (mapGen.objs.Count == 0)
+        {         
+            objs = mapGen.objs;
+        }
+        
+        foreach (var obj in objs)
+        {
+            if (obj == null)
+            {
+                continue;
+            }
+            var block = obj.GetComponent<BreakBlock>();
+            // コンポーネントがないオブジェクトをはじく
+            if (block != null)
+            {
+                block.BreackUpdate();
+            }
+        }
+    }
+}
