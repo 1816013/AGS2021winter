452f9613510d8fe426d8c546745c4b0a99b5b852
RyuseiKotou <49849206+RyuseiKotou@users.noreply.github.com>
2021-02-24 14:40:25 +0900

    main

---
+++ AGS2021winter/Assets/Script/MainModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainModel : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Transform model = this.transform;
+
+        Vector3 rotation = model.eulerAngles;
+        rotation.y += 0.05f;
+        model.eulerAngles = rotation;
+    }
+}
